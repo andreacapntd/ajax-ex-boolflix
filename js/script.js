b77693e69aa74b6ceb815b8c4672c7242fce5fe6
andreacapntd <andreacapntd@gmail.com>
2020-07-27 14:24:52 +0200

    api ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+function  addListenerStartFilmSearch() {
+
+  var button = $('#btn');
+
+  button.click(filmSearch);
+
+};
+
+function filmSearch() {
+
+  var inputTarget = $('#search');
+
+  var template = $('#film_template').html();
+  var compiled = Handlebars.compile(template);
+  var target = $('#films');
+  target.html('');
+
+  $.ajax({
+
+    url:'https://api.themoviedb.org/3/search/movie?api_key=f8286655b8fe2d5049fce0ac4760805f',
+    method: 'GET',
+    data: {
+
+      'query': inputTarget.val()
+    },
+    success: function(data) {
+
+      var success = data['success'];
+      var films = data['results'];
+      console.log(films);
+
+    },
+    error: function(err) {
+
+      console.log('err', err);
+
+    }
+
+
+  })
+
+
+};
 
 
 
@@ -13,6 +56,15 @@
 
 
 
+function test() {
+
+
+  button.click(function(){
+
+    var inputValue = inputTarget.val();
+    console.log(inputValue);
+  })
+}
 
 
 
@@ -23,8 +75,9 @@
 
 function init() {
 
-  console.log('hello');
-  
+  addListenerStartFilmSearch();
+
+  // test ();
 }
 
 
