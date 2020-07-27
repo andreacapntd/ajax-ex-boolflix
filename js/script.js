81c4ea14038d12371713268bfd8c408116d297a3
andreacapntd <andreacapntd@gmail.com>
2020-07-27 15:09:31 +0200

    milestone 1 ok

--- js/script.js
+++ js/script.js
@@ -12,12 +12,13 @@ function filmSearch() {
 
   var template = $('#film_template').html();
   var compiled = Handlebars.compile(template);
-  var target = $('#films');
-  target.html('');
+  var filmsTarget = $('#films');
+
+  filmsTarget.html('');
 
   $.ajax({
 
-    url:'https://api.themoviedb.org/3/search/movie?api_key=f8286655b8fe2d5049fce0ac4760805f',
+    url:'https://api.themoviedb.org/3/search/movie?api_key=f8286655b8fe2d5049fce0ac4760805f&language=it-IT',
     method: 'GET',
     data: {
 
@@ -26,8 +27,28 @@ function filmSearch() {
     success: function(data) {
 
       var success = data['success'];
-      var films = data['results'];
-      console.log(films);
+      var filmsResults = data['results'];
+
+      for (var i = 0; i < filmsResults.length; i++) {
+        var films = filmsResults[i];
+        var filmTitle = films['title'];
+        var filmOriginalTitle = films['original_title'];
+        var filmLanguage = films['original_language'];
+        var filmRate = films['vote_average'];
+
+        var filmsHtml = compiled({
+
+          'title': filmTitle,
+          'originalTitle': filmOriginalTitle,
+          'language': filmLanguage,
+          'rate': filmRate
+
+        })
+
+        inputTarget.val('');
+        filmsTarget.append(filmsHtml);
+
+      }
 
     },
     error: function(err) {
